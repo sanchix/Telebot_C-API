6f73104c2a081669ae5dbb9952afc49f01abaadc
Javier Sanchidrian <jsabo@hotmail.es>
2020-12-07 19:27:03 +0100

    Test with json

--- lib/json/example/simple.c
+++ lib/json/example/simple.c
@@ -8,9 +8,7 @@
  * tokens is predictable.
  */
 
-static const char *JSON_STRING =
-    "{\"user\": \"johndoe\", \"admin\": false, \"uid\": 1000,\n  "
-    "\"groups\": [\"users\", \"wheel\", \"audio\", \"video\"]}";
+static const char *JSON_STRING = "{\"ok\":true,\"result\":[{\"update_id\":596179679,\"message\":{\"message_id\":16,\"from\":{\"id\":150848014,\"is_bot\":false,\"first_name\":\"Javi\",\"last_name\":\"Sanchi\",\"language_code\":\"en\"},\"chat\":{\"id\":150848014,\"first_name\":\"Javi\",\"last_name\":\"Sanchi\",\"type\":\"private\"},\"date\":1607340555,\"text\":\"Parada claro\"}},{\"update_id\":596179680,\"message\":{\"message_id\":17,\"from\":{\"id\":150848014,\"is_bot\":false,\"first_name\":\"Javi\",\"last_name\":\"Sanchi\",\"language_code\":\"en\"},\"chat\":{\"id\":150848014,\"first_name\":\"Javi\",\"last_name\":\"Sanchi\",\"type\":\"private\"},\"date\":1607340661,\"text\":\"Ho\"}}]}";
 
 static int jsoneq(const char *json, jsmntok_t *tok, const char *s) {
   if (tok->type == JSMN_STRING && (int)strlen(s) == tok->end - tok->start &&
@@ -25,53 +23,42 @@ int main() {
   int r;
   jsmn_parser p;
   jsmntok_t t[128]; /* We expect no more than 128 tokens */
+  char buffer[100];
 
   jsmn_init(&p);
   r = jsmn_parse(&p, JSON_STRING, strlen(JSON_STRING), t,
-                 sizeof(t) / sizeof(t[0]));
+				 sizeof(t) / sizeof(t[0]));
   if (r < 0) {
-    printf("Failed to parse JSON: %d\n", r);
-    return 1;
+	printf("Failed to parse JSON: %d\n", r);
+	return 1;
   }
 
   /* Assume the top-level element is an object */
   if (r < 1 || t[0].type != JSMN_OBJECT) {
-    printf("Object expected\n");
-    return 1;
+	printf("Object expected\n");
+	return 1;
   }
 
   /* Loop over all keys of the root object */
   for (i = 1; i < r; i++) {
-    if (jsoneq(JSON_STRING, &t[i], "user") == 0) {
-      /* We may use strndup() to fetch string value */
-      printf("- User: %.*s\n", t[i + 1].end - t[i + 1].start,
+	  
+	printf("%i\n", t[i].type);
+	if(jsoneq(JSON_STRING, &t[i], "ok") == 0){
+		printf(">ok: %.*s\n", t[i + 1].end - t[i + 1].start,
              JSON_STRING + t[i + 1].start);
-      i++;
-    } else if (jsoneq(JSON_STRING, &t[i], "admin") == 0) {
-      /* We may additionally check if the value is either "true" or "false" */
-      printf("- Admin: %.*s\n", t[i + 1].end - t[i + 1].start,
-             JSON_STRING + t[i + 1].start);
-      i++;
-    } else if (jsoneq(JSON_STRING, &t[i], "uid") == 0) {
-      /* We may want to do strtol() here to get numeric value */
-      printf("- UID: %.*s\n", t[i + 1].end - t[i + 1].start,
-             JSON_STRING + t[i + 1].start);
-      i++;
-    } else if (jsoneq(JSON_STRING, &t[i], "groups") == 0) {
-      int j;
-      printf("- Groups:\n");
-      if (t[i + 1].type != JSMN_ARRAY) {
-        continue; /* We expect groups to be an array of strings */
-      }
-      for (j = 0; j < t[i + 1].size; j++) {
-        jsmntok_t *g = &t[i + j + 2];
-        printf("  * %.*s\n", g->end - g->start, JSON_STRING + g->start);
-      }
-      i += t[i + 1].size + 1;
-    } else {
-      printf("Unexpected key: %.*s\n", t[i].end - t[i].start,
-             JSON_STRING + t[i].start);
-    }
+	}
+	if(jsoneq(JSON_STRING, &t[i], "result") == 0){
+		printf(">result:\n");
+		int offset = 0;
+		for(int j = 0; j < t[i+1].size; i++){
+			
+			if(jsoneq(JSON_STRING, &t[i+3+offset], "update_id") == 0){
+				
+			}
+			
+		}
+	}
+	
   }
   return EXIT_SUCCESS;
 }

99b9e065f67ea8f1117ac94953c6b39bde5c381c
J4V1R6 <javirosra@gmail.com>
2020-12-12 11:24:15 +0100

    Incluido json en make

--- lib/json/example/simple.c
+++ lib/json/example/simple.c
@@ -12,7 +12,8 @@
 static const char *JSON_STRING = "{\"ok\":true,\"result\":[{\"update_id\":596179679,\"message\":{\"message_id\":16,\"from\":{\"id\":150848014,\"is_bot\":false,\"first_name\":\"Javi\",\"last_name\":\"Sanchi\",\"language_code\":\"en\"},\"chat\":{\"id\":150848014,\"first_name\":\"Javi\",\"last_name\":\"Sanchi\",\"type\":\"private\"},\"date\":1607340555,\"text\":\"Parada claro\"}},{\"update_id\":596179680,\"message\":{\"message_id\":17,\"from\":{\"id\":150848014,\"is_bot\":false,\"first_name\":\"Javi\",\"last_name\":\"Sanchi\",\"language_code\":\"en\"},\"chat\":{\"id\":150848014,\"first_name\":\"Javi\",\"last_name\":\"Sanchi\",\"type\":\"private\"},\"date\":1607340661,\"text\":\"Ho\"}}]}";
 */
 
-static const char *JSON_STRING = "{\"nombre\":\"valor\",\"lista\":{\"nombre2\":\"valor2\", \"lista2\":{\"a\":\"1\",\"b\":\"2\"},\"nombre3\":\"valor3\"}}";
+//static const char *JSON_STRING = "{\"nombre\":\"valor\",\"objeto\":{\"nombre2\":\"valor2\", \"objeto2\":{\"a\":\"1\",\"b\":\"2\"},\"nombre3\":\"valor3\"}}";
+static const char *JSON_STRING = "{\"nombre\":\"valor\",\"lista\":[\"valor2\",\"valor3\"],\"lista2\":[\"valor4\", \"objeto1\":{\"a\":\"1\",\"b\":\"2\"}]}";
 
 int analize_object(jsmntok_t *t){
 	
@@ -22,15 +23,19 @@ int analize_object(jsmntok_t *t){
 	printf("{\n");
 	
 	for(int i = 0 ; i < t[0].size ; i++){
-		printf("%.*s:\n", t[offset].end - t[offset].start,
-             JSON_STRING + t[offset].start);
+		
+    printf("%.*s:\n", t[offset].end - t[offset].start, JSON_STRING + t[offset].start);
+
 		if(t[offset+1].type == JSMN_STRING){
-			printf("%.*s\n", t[offset + 1].end - t[offset + 1].start,
-            JSON_STRING + t[offset + 1].start);
+
+			printf("%.*s\n", t[offset + 1].end - t[offset + 1].start, JSON_STRING + t[offset + 1].start);
+
 			offset = offset + 2; 
 		}
 		else if(t[offset+1].type == JSMN_OBJECT){
+
 			offset = offset + 1 + analize_object(&t[offset+1]);
+
 		}
 	}
 	
@@ -39,6 +44,39 @@ int analize_object(jsmntok_t *t){
 	tokensize = offset;
 	return tokensize;
 	
+}
+
+int analize_list(jsmntok_t *t){
+  
+  int tokensize = 0;
+  int offset = 1;
+  
+  printf("{\n");
+  
+  for(int i = 0 ; i < t[0].size ; i++){
+
+    if(t[offset].type == JSMN_STRING){
+
+      printf("%.*s\n", t[offset].end - t[offset].start, JSON_STRING + t[offset].start);
+
+      offset = offset + 1; 
+    }
+    else if(t[offset].type == JSMN_OBJECT){
+
+      offset = offset + analize_object(&t[offset+1]);
+      
+    }else if(t[offset].type == JSMN_ARRAY){
+
+      offset = offset + analize_object(&t[offset+1]);
+      
+    }
+  }
+  
+  printf("}\n");
+  
+  tokensize = offset;
+  return tokensize;
+  
 }
 
 static int jsoneq(const char *json, jsmntok_t *tok, const char *s) {
@@ -75,17 +113,25 @@ int main() {
 	  
 	//printf("%i\n", t[i].type);
 	if(t[i].type == JSMN_STRING){
-		printf("%.*s:\n", t[i].end - t[i].start,
-             JSON_STRING + t[i].start);
+		
+    printf("%.*s:\n", t[i].end - t[i].start, JSON_STRING + t[i].start);
+
 		if(t[i+1].type == JSMN_STRING){
-			printf("%.*s\n", t[i + 1].end - t[i + 1].start,
-             JSON_STRING + t[i + 1].start);
+
+			printf("%.*s\n", t[i + 1].end - t[i + 1].start,JSON_STRING + t[i + 1].start);
 			 i = i + 1;
-		}
-		else if(t[i+1].type == JSMN_OBJECT){
-			int offset = analize_object(&t[i+1]);
+
+		}else if(t[i+1].type == JSMN_OBJECT){
+		
+    	int offset = analize_object(&t[i+1]);
 			i = i + offset;
-		}
+		
+    }else if(t[i+1].type == JSMN_ARRAY){
+    
+      int offset = analize_list(&t[i+1]);
+      i = i + offset;
+    
+    }
 	}
 	
   }

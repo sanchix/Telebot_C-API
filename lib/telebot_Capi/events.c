a97a2b5194e8d2addca1bba944a7b4acde7336ee
Javier Sanchidrian <jsabo@hotmail.es>
2020-12-19 13:20:26 +0100

    Implement event structure to pooling.c

--- lib/telebot_Capi/events.c
+++ lib/telebot_Capi/events.c
@@ -22,6 +22,11 @@
 /************************************************************/
 /* Definiciones de funciones  */
 
+
+// TODO: Comentar
+void *doNothing(void *p){/*do nothing absolutely*/ return NULL; }
+
+
 /*
 **   Parámetros:  <tipo1> <parm1> <Descripción>
 **                <tipo2> <parm2> <Descripción>
@@ -35,22 +40,54 @@ void initUpdateEvents(event_t *updateEvents){
 	
 	// TODO: Set first update handler to NULL (don't change offset on HTTP request), and others to "doNothing()"
 	updateHandlers[0].handler = NULL;
-	for(i in(1,MAX_UPDATE_HANDLERS){
-		uH[i].handler = doNothing();
+	updateHandlers[0].condition = CONDITION_UNASSIGNED;
+	for(int i = 1; i < MAX_UPDATE_HANDLERS; i++){
+		updateHandlers[i].handler = doNothing;
+		updateHandlers[i].condition = CONDITION_UNASSIGNED;
 	}
 	
 }
 
-// Esta función debe bloquear los eventos (hay que poner un mutex)
-void addUpdateEvent(){
-	//TODO: Poner exclusión mutua
-	//bajar
+
+// TODO: Comentar: Esta función debe bloquear los eventos (hay que poner un mutex)
+// TODO: Cambiar el parametro newEvent por algo más userfriendly? Probablemente con hacer event_t más userfrienly sea suficiente...
+int addUpdateEvent(event_t *updateEvents, event_t newEvent){
 	
-	//subir
+	int found = 0;
+	
+	// TODO: Poner exclusión mutua
+	//bajar
+	for(int i = 1; i < MAX_UPDATE_HANDLERS && !found; i++){
+		// Cuando encontremos uno vacío lo usamos
+		if(updateEvents[i].condition == CONDITION_UNASSIGNED){
+			updateEvents[i].handler = newEvent.handler;
+			updateEvents[i].condition = newEvent.condition;
+			found = 1;
+		}
+	}
+	// subir
+	
+	return found-1
 }
 
-void removeUpdateEvent(){
+
+// TODO: Comentar
+int removeUpdateEvent(event_t *updateEvents, event_t event){
 	
+	int found = 0;
 	
+	// TODO: Poner exclusión mutua
+	//bajar
+	for(i in(1, MAX_UPDATE_HANDLERS) && !found){
+		// TODO: Cambiar este "found" a algo que diga si son iguales (¿por una comparación hash?, ¿elemento a elemento?)
+		if(found){
+			updateEvents[i].handler = doNothing();
+			updateEvents[i].condition = CONDITION_UNASSIGNED;
+			found = 1;
+		}
+	}
+	// subir
+	
+	return found-1
 	
 }
